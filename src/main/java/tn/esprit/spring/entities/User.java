f22267966ce6045d98fae83587b47a7ff9a4a39a
asmaRadhouane <asma.radhouane@esprit.tn>
2023-10-05 15:49:02 +0200

    third commit

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -21,7 +21,7 @@ public class User implements Serializable {
 
 
 	private static final long serialVersionUID = 1L;
-
+//third change
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id;   
